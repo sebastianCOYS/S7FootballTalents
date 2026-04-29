a7736a147120f505bcd7202f06e84fec87ff20ff
sebastianCOYS <sebik.darkos@gmail.com>
2026-04-29 14:41:05 +0200

    fixed radarChart on vertical mobile too small

--- src/components/PlayerRadarChart.tsx
+++ src/components/PlayerRadarChart.tsx
@@ -7,19 +7,19 @@ type PlayerRadarChartProps = {
 
 export default function PlayerRadarChart({player,chartType}: PlayerRadarChartProps) {
     if (chartType === "offensive") {
-        return <RadarChart height={300}
+        return <RadarChart sx={{minWidth: "300px"}} height={300}
                 series={[{ label: player.Player, data: [player.Gls, player["G-PK"], player.Ast, player.xG, player.npxG, player.xAG, player["G+A"], player.Carries,  player.PrgP, player.PrgC], fillArea: true },]}
                 radar={{metrics: ['Goals', 'Goals-PK', 'Assists', 'xG','npxG', 'xAG', 'G+A', 'Carries', 'Progressive p.', 'Progressive c.','key passes']}}
                 />
     }
     if (chartType === "defensive") {
-        return <RadarChart height={300}
+        return <RadarChart sx={{minWidth: "300px"}} height={300}
                 series={[{ label: player.Player, data: [player.TklW, player.Int, player.Clr, player.Recov, player.CrdY, player.CrdR], fillArea: true },]}
                 radar={{metrics: ['Tackles Won', 'Interceptions', 'Clearances', 'Recoveries', 'Yellow C.', 'Red C.']}}
                 />
     }
     if (chartType === "goalkeeping") {
-        return <RadarChart height={300}
+        return <RadarChart sx={{minWidth: "300px"}} height={300}
                 series={[{ label: player.Player, data: [Number(player.GA), Number(player.GA90), Number(player.Saves), Number(player["Save%"]), player.CK, Number(player.PKsv), Number(player["Cmp%"])], fillArea: true },]}
                 radar={{metrics: ['Conceded', 'Condeded/90m', 'Saves', 'save%','clean sheets', 'penalties saved', 'pass completion%']}}
                 />
